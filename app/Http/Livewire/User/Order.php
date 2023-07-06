cbf6725305f0d2fff33e5592974f53400501dc09
iwanekaputra <iwanekaputra5@gmail.com>
2023-07-06 10:29:22 +0700

    page pesanan

--- app/Http/Livewire/User/Order.php
+++ app/Http/Livewire/User/Order.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Transaction;
 use Livewire\Component;
 
 class Order extends Component
 {
+
+    public function mount(){
+
+    }
+
     public function render()
     {
-        return view('livewire.user.order')->extends('layouts.app');
+        return view('livewire.user.order', [
+            'transactions' => Transaction::where('user_id', auth()->user()->id)->get()
+        ])->extends('layouts.app');
     }
 }
